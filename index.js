22c0d7df058cbaa9b9642cc088884e2a237608ad
Abhishek Dhanani <abhishekdhanani0931@gmail.com>
2025-09-19 20:20:25 +0530

    reverting

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import { redirect } from './controllers/urlController.js';
 dotenv.config();
 await connectDB();
 
+
 const app = express();
 
 app.use(cors({
@@ -19,7 +20,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static('uploads')); // Serve static files from 'uploads' directory
 
 app.use('/api/users', userRoutes);
@@ -29,7 +29,9 @@ app.get('/:code', redirect);
 app.get('/', (req, res) => res.send('shortify backend running...'));
 app.get('/api', (req, res) => res.send('API is running...'));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-export default app;
